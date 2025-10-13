e419dbf98df7adec3b8787fce2c9075bdd187168
NEVVY <nevvy@qq.com>
2025-10-14 02:07:17 +0800

    1

--- local_inference.py
+++ local_inference.py
@@ -223,8 +223,10 @@ def mt_dialogue_gen(data_path,model_path,result_path):
                 model_prompt = data["model_prompt"]
                 
                 print("\n----------------------------------- model_prompt\n" + model_prompt)
+
                 gpt = GPTPerson(data=data,model_name=args.model_name,gpt_url=args.api_url,api_key=args.api_key)
                 query = gpt.initial_response()#role prompt要求GPT角色扮演用户
+                # query=data["needs"]
                 dialogue = dialogue + "用户：" + query + "\n"
                 print("\n-----------------------------------first query\n"+query)
                 history = []
